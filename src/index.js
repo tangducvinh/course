fd9095041004b2438fe71fe7c0817bb10defdc9c
vinh <ducvinh100503@gmail.com>
2023-08-14 17:26:01 +0700

    day 1

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars');
+const app = express();
+const port = 3000
+
+app.use(morgan('combined'))
+
+app.engine('hbs', exphbs({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', './src/resources/views');
+
+app.get('/', (req, res) => {
+    return res.render('home')
+})
+
+app.get('/news', (req, res) => {
+    return res.render('news')
+})
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
